238f044f62ae100632c61ea5e2bad92214aa452c
chrisbmx20 <calvarez@fwdcostarica.com>
2024-08-20 11:53:14 -0600

    primer commit

---
+++ services/usuario/delete-usuario.js
@@ -0,0 +1,21 @@
+async function deleteUser(id) {
+    try {
+        const response = await fetch(`http://localhost:3001/users/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error deleting user with id ${id}`);
+        }
+
+        return { message: `User with id ${id} deleted successfully` };
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+}
+
+export { deleteUser };
---
+++ services/usuario/put-usuario.js
@@ -0,0 +1,26 @@
+async function updateUsers(nombre, apellido,id) {
+    try {
+     
+        const userData = { 
+            nombre, 
+            apellido 
+        
+        };
+        const response = await fetch("http://localhost:3001/users/"+id, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+     
+        return await response.json();
+    } catch (error) {
+        console.error('Error update user:', error);
+        throw error;
+    }
+}
+
+export{updateUsers}
+
