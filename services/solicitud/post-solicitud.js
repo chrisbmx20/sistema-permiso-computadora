847dc26a13e6225f2a9c19102f87301204e25d6f
chrisbmx20 <calvarez@fwdcostarica.com>
2024-08-21 14:33:45 -0600

    login correcto

---
+++ services/solicitud/post-solicitud.js
@@ -0,0 +1,35 @@
+
+
+async function postPeticiones(usuario) {
+    try {
+     
+        const userData = { 
+    "id": usuario.id,
+    "nombre": usuario.nombre,
+    "apellido": usuario.apellido,
+    "correo": usuario.correo,
+    "telefono": usuario.telefono,
+    "role": usuario.role,
+    "password": usuario.password
+        
+        };
+
+        const response = await fetch("http://localhost:3000/usuarios", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+     
+        return await response.json();
+
+        
+    } catch (error) {
+        console.error('Error posting user:', error);
+        throw error;
+    }
+}
+
+export{postPeticiones}
